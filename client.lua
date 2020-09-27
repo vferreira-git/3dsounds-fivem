89a63fe0f0b987711a9630bd974ffef8f6242723
vferreira-git <56931107+vferreira-git@users.noreply.github.com>
2020-09-27 13:28:42 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,12 +1,3 @@
-ESX = nil
-Sounds = {}
-
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-end)
 
 Citizen.CreateThread(function()
     while true do
